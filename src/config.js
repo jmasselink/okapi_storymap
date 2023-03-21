2865476d501b890b820d70bc9b7c1b52f2a7d573
Joel M <jmasselink@gmail.com>
2023-03-20 22:50:58 -0400

    added basic structure

--- src/config.js
+++ src/config.js
@@ -10,8 +10,8 @@ var config = {
     theme: 'dark',
     use3dTerrain: true, //set true for enabling 3D maps.
     auto: false,
-    title: 'The Title Text of this Story',
-    subtitle: 'A descriptive and interesting subtitle to draw in the reader',
+    title: 'If a mine grows in the forest...',
+    subtitle: '...and no one pays attention',
     byline: 'By a Digital Storyteller',
     footer: 'Source: source citations, etc. <br> Created using <a href="https://github.com/mapbox/storytelling" target="_blank">Mapbox Storytelling</a> template.',
     chapters: [
@@ -21,7 +21,7 @@ var config = {
             hidden: false,
             title: 'Display Title',
             image: './path/to/image/source.png',
-            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+            description: 'The Democraticd do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
             location: {
                 center: [21, -3],
                 zoom: 4,
@@ -60,12 +60,75 @@ var config = {
                     layer: 'rfo-limits-natural',
                     opacity: 0,
                 },
+                {
+                    layer: 'rfo-cami',
+                    opacity: 0,
+                },
+                {
+                    layer: 'settlement-major-label',
+                    opacity: 0.0,
+                },
+                {
+                    layer: 'settlement-minor-label',
+                    opacity: 0.0,
+                },
+                {
+                    layer: 'unesco-sites',
+                    opacity: 0.0,
+                },
+                {
+                    layer: 'poi-label',
+                    opacity: 0.0,
+                },
             ],
             onChapterExit: [
                 // {
                 //     layer: 'layer-name',
                 //     opacity: 0
                 // }
+            ]
+        },
+        {
+            id: 'unesco',
+            alignment: 'right',
+            hidden: false,
+            title: '0. UNESCO',
+            image: './path/to/image/source.png',
+            description: 'Copy these sections to add to your story.',
+            location: {
+                    center: [24, -1],
+                    zoom: 6,
+                    pitch: 20,
+                    bearing: 0,
+                // flyTo additional controls-
+                // These options control the flight curve, making it move
+                // slowly and zoom out almost completely before starting
+                // to pan.
+                //speed: 2, // make the flying slow
+                //curve: 1, // change the speed at which it zooms out
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [
+                {
+                    layer: 'unesco-sites',
+                    opacity: 0.8,
+                },
+                {
+                    layer: 'landcover',
+                    opacity: 0.2, 
+                },
+            ],
+            onChapterExit: [
+                {
+                    layer: 'rfo-limits-polygon',
+                    opacity: 0.4,
+                },
+                {
+                    layer: 'unesco-sites',
+                    opacity: 0.1,
+                },
             ]
         },
         {
@@ -218,6 +281,10 @@ var config = {
             callback: '',
             onChapterEnter: [
                 {
+                    layer: 'rfo-cami',
+                    opacity: 0.8,
+                },
+                {
                     layer: 'rfo-limits-polygon',
                     opacity: 0.2,
                 },
@@ -242,12 +309,7 @@ var config = {
                     opacity: 0.0,
                 },
             ],
-            onChapterExit: [
-                // {
-                //     layer: 'rfo-limits-polygon',
-                //     opacity: 0.4,
-                // },
-            ]
+            onChapterExit: []
         },
         {
             id: 'ch5',
