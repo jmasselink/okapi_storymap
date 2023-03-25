9b87af3286f88f619bc9a7b2d15b1a7c6277ae3b
Joel M <jmasselink@gmail.com>
2023-03-25 16:35:23 -0400

    major structure update

--- src/config.js
+++ src/config.js
@@ -11,7 +11,7 @@ var config = {
     use3dTerrain: true, //set true for enabling 3D maps.
     auto: false,
     title: 'If a mine grows in the forest...',
-    subtitle: '...and no one pays attention',
+    // subtitle: '...and no one pays attention',
     byline: 'By a Digital Storyteller',
     footer: 'Source: source citations, etc. <br> Created using <a href="https://github.com/mapbox/storytelling" target="_blank">Mapbox Storytelling</a> template.',
     chapters: [
@@ -19,9 +19,9 @@ var config = {
             id: 'intro',
             alignment: 'left',
             hidden: false,
-            title: 'Display Title',
+            title: '0. DRC: a megadiverse nation of natural wealth',
             image: './path/to/image/source.png',
-            description: 'The Democraticd do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+            description: 'The Democratic Republic of the Congo is blessed with biodiversity, abundant natural resources, and diverse habitats. Home to nearly a hundred million people, the DRC also has hundreds of ethnic groups. Even within the country, there are 4 primary national languages: Kikongo, Kiluba, Kiswahili, and Lingala.',
             location: {
                 center: [21, -3],
                 zoom: 4,
@@ -40,14 +40,18 @@ var config = {
                     layer: 'admin-1-boundary-bg',
                     opacity: 0,
                 },
-                // {
-                //     layer: 'landuse-overlay-outline',
-                //     opacity: 0,
-                // },
-                // {
-                //     layer: 'landuse-overlay',
-                //     opacity: 0,
-                // },
+                {
+                    layer: 'active-exploitation-line',
+                    opacity: 0,
+                },
+                {
+                    layer: 'active-exploration-line',
+                    opacity: 0,
+                },
+                {
+                    layer: 'research-application-line',
+                    opacity: 0,
+                },
                 {
                     layer: 'rfo-limits-polygon',
                     opacity: 0,
@@ -89,17 +93,17 @@ var config = {
             ]
         },
         {
-            id: 'unesco',
+            id: 'ch1',
             alignment: 'right',
             hidden: false,
-            title: '0. UNESCO',
+            title: '1. Ituri Forest: a refuge for indigenous livelihoods and wildlife alike',
             image: './path/to/image/source.png',
-            description: 'Copy these sections to add to your story.',
+            description: 'A refuge for indigenous livelihoods and wildlife alike.',
             location: {
-                    center: [24, -1],
-                    zoom: 6,
-                    pitch: 20,
-                    bearing: 0,
+                center: [28.531101, 1.531721],
+                zoom: 7,
+                pitch: 5,
+                bearing: 0
                 // flyTo additional controls-
                 // These options control the flight curve, making it move
                 // slowly and zoom out almost completely before starting
@@ -111,10 +115,10 @@ var config = {
             rotateAnimation: false,
             callback: '',
             onChapterEnter: [
-                {
-                    layer: 'unesco-sites',
-                    opacity: 0.8,
-                },
+                // {
+                //     layer: 'rfo-limits-polygon',
+                //     opacity: 0.6,
+                // },
                 {
                     layer: 'landcover',
                     opacity: 0.2, 
@@ -125,17 +129,13 @@ var config = {
                     layer: 'rfo-limits-polygon',
                     opacity: 0.4,
                 },
-                {
-                    layer: 'unesco-sites',
-                    opacity: 0.1,
-                },
             ]
         },
         {
-            id: 'ch0',
+            id: 'ch2',
             alignment: 'right',
             hidden: false,
-            title: '0. RFO',
+            title: '2. Delicate trust and fragile peace',
             image: './path/to/image/source.png',
             description: 'Copy these sections to add to your story.',
             location: {
@@ -171,10 +171,10 @@ var config = {
             ]
         },
         {
-            id: 'ch1',
+            id: 'ch3',
             alignment: 'left',
             hidden: false,
-            title: '1. Western Boundary',
+            title: '3. Boundaries both real and imaginary',
             image: './path/to/image/source.png',
             description: 'Copy these sections to add to your story.',
             location: {
@@ -200,10 +200,10 @@ var config = {
             ]
         },
         {
-            id: 'ch2',
+            id: 'ch4',
             alignment: 'left',
             hidden: false,
-            title: '2. Title',
+            title: '4. Challenges in the Reserve',
             image: './path/to/image/source.png',
             description: 'Copy these sections to add to your story.',
             location: {
@@ -229,10 +229,10 @@ var config = {
             ]
         },
         {
-            id: 'ch3',
+            id: 'ch5',
             alignment: 'fully',
             hidden: false,
-            title: '3. Title',
+            title: '5. An erroneous map changes the balance',
             image: './path/to/image/source.png',
             description: 'Copy these sections to add to your story.',
             location: {
@@ -258,10 +258,10 @@ var config = {
             ]
         },
         {
-            id: 'ch4',
+            id: 'ch6',
             alignment: 'right',
             hidden: false,
-            title: '4. Title',
+            title: '6. The incongruence of mining concessions in a World Heritage site',
             image: './path/to/image/source.png',
             description: 'Copy these sections to add to your story.',
             location: {
@@ -312,11 +312,11 @@ var config = {
             onChapterExit: []
         },
         {
-            id: 'ch5',
+            id: 'ch7',
             alignment: 'fully',
             hidden: false,
-            title: '5. Mines',
-            image: './path/to/image/source.png',
+            title: '7. Expansion of semi-industrial causing permanent damages',
+            image: '/assets/rfo_gfw_alerts_confidence.gif',
             description: 'Copy these sections to add to your story.',
             location: {
                 center: [28.131101, 1.331721],
@@ -339,6 +339,49 @@ var config = {
                 //     opacity: 0.6,
                 // },  
             ]
-        }
+        },
+                // {
+        //     id: 'unesco',
+        //     alignment: 'left',
+        //     hidden: false,
+        //     title: '0. World Heritage in Danger',
+        //     image: './path/to/image/source.png',
+        //     description: 'Copy these sections to add to your story.',
+        //     location: {
+        //             center: [24, -1],
+        //             zoom: 6,
+        //             pitch: 20,
+        //             bearing: 0,
+        //         // flyTo additional controls-
+        //         // These options control the flight curve, making it move
+        //         // slowly and zoom out almost completely before starting
+        //         // to pan.
+        //         //speed: 2, // make the flying slow
+        //         //curve: 1, // change the speed at which it zooms out
+        //     },
+        //     mapAnimation: 'flyTo',
+        //     rotateAnimation: false,
+        //     callback: '',
+        //     onChapterEnter: [
+        //         {
+        //             layer: 'unesco-sites',
+        //             opacity: 0.8,
+        //         },
+        //         {
+        //             layer: 'landcover',
+        //             opacity: 0.2, 
+        //         },
+        //     ],
+        //     onChapterExit: [
+        //         {
+        //             layer: 'rfo-limits-polygon',
+        //             opacity: 0.4,
+        //         },
+        //         {
+        //             layer: 'unesco-sites',
+        //             opacity: 0.1,
+        //         },
+        //     ]
+        // },
     ]
 };
