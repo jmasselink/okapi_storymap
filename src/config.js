79cdccda7d8b7908aef1f203c145099485860284
Joel M <jmasselink@gmail.com>
2023-04-02 19:50:45 -0400

    added photos & fixed text

--- src/config.js
+++ src/config.js
@@ -2,18 +2,18 @@ var config = {
     style: 'mapbox://styles/jmasselink/cldzeoc0g005e01lc5830y7pr',
     accessToken: 'pk.eyJ1Ijoiam1hc3NlbGluayIsImEiOiJjbGU3eWllcmcwYTJnM29taG51dnV4NDhxIn0.fvnE_Pf3T6kKOt7TUeuubw',
     NICFI_API_KEY: 'PLAK4a3933f7143c4215ba8d61bf91d397d4',
-    showMarkers: true,
+    showMarkers: false,
     markerColor: '#3FB1CE',
     projection: 'globe',//'equirectangular',
     //Read more about available projections here
     //https://docs.mapbox.com/mapbox-gl-js/example/projections/
     inset: true,
-    theme: 'light',
+    theme: 'dark',
     use3dTerrain: true, //set true for enabling 3D maps.
     auto: false,
-    title: 'If a mine grows in the forest...',
+    title: 'How a mine grows in the forest...',
     // subtitle: '...and no one pays attention',
-    byline: 'By a Digital Storyteller',
+    // byline: 'By a Digital Storyteller',
     footer: 'Source: source citations, etc. <br> Created using <a href="https://github.com/mapbox/storytelling" target="_blank">Mapbox Storytelling</a> template.',
     chapters: [
         {
@@ -22,7 +22,7 @@ var config = {
             hidden: false,
             title: '1. DRC: a megadiverse nation of natural wealth',
             // image: './path/to/image/source.png',
-            description: 'The Democratic Republic of the Congo is blessed with biodiversity, abundant natural resources, and diverse habitats. Home to an estimated hundred million people, the DRC has young, rapidly growing population, which represent hundreds of ethnic groups. Despite this, DRC ranks near the bottom (179/191) on the <a href="https://hdr.undp.org/data-center/specific-country-data#/countries/COD">Human Development Index</a> and most of DRC’s people live in extreme poverty.',
+            description: "The Democratic Republic of the Congo is blessed with biodiversity, abundant natural resources, and diverse habitats. Home to an estimated hundred million people, the DRC has young, rapidly growing population, which represent hundreds of ethnic groups. Despite this, DRC ranks near the bottom (179/191) on the <a href='https://hdr.undp.org/data-center/specific-country-data#/countries/COD'>Human Development Index</a> and most of DRC’s people live in extreme poverty.",
             location: {
                 center: [21, -3],
                 zoom: 4,
@@ -131,8 +131,8 @@ var config = {
             alignment: 'right',
             hidden: false,
             title: '2. The Ituri Forest and Indigenous Communities',
-            // image: './path/to/image/source.png',
-            description: 'The Ituri forest, located in northeastern DRC, is home to the Mbuti and Efe indigenous communities, who have lived there for thousands of years. They practice traditional ceremonies, subsistence hunting and gathering, and temporary settlements deep within the forest or on the outskirts of villages. These communities have a deep respect for the okapi, a close relative of the giraffe, which primarily inhabits the dense, lowland areas of the Ituri forest. Despite being skilled hunters, they avoid hunting the okapi out of reverence for this animal.',
+            image: '/assets/JoelMasselink_Mbuti_Girls2.JPG',
+            description: "The Ituri forest, located in northeastern DRC, is home to the Mbuti and Efe indigenous communities, who have lived there for thousands of years. They practice traditional ceremonies, subsistence hunting and gathering, and temporary settlements deep within the forest or on the outskirts of villages. These communities have a deep respect for the okapi, a close relative of the giraffe, which primarily inhabits the dense, lowland areas of the Ituri forest. Despite being skilled hunters, they avoid hunting the okapi out of reverence for this animal.",
             location: {
                 center: [28.531101, 1.531721],
                 zoom: 7,
@@ -167,8 +167,8 @@ var config = {
             alignment: 'right',
             hidden: false,
             title: '3. Creation of the Okapi Wildlife Reserve',
-            // image: './path/to/image/source.png',
-            description: 'The Okapi Wildlife Reserve was established in 1992 to protect the unique way of life of indigenous peoples, wildlife, and the Ituri forest ecosystem. A mixed-use protected area, the Okapi Reserve protects 13,726 sq km, an area larger than Death Valley National Park, slightly smaller than Connecticut or Montenegro. The reserve protects the rights of access to arable land for indigenous Bantu horticulturalist communities as well. <p>In 1996, Okapi Wildlife Reserve was named a UNESCO World Heritage Site, by virtue of it containing the most important and significant natural habitats for in-situ biodiversity conservation of threatened species. The reserve harbors globally important populations of African forest elephant, okapi, and eastern chimpanzees, as well as 13 other species of diurnal primate and four nocturnal species, ranking it as the top protected area in sub-Saharan Africa in terms of primate diversity',
+            image: '/assets/JoelMasselink_Okapi.JPG',
+            description: "The Okapi Wildlife Reserve was established in 1992 to protect the unique way of life of indigenous peoples, wildlife, and the Ituri forest ecosystem. A mixed-use protected area, the Okapi Reserve protects 13,726 sq km, an area larger than Death Valley National Park, slightly smaller than Connecticut or Montenegro. The reserve protects the rights of access to arable land for indigenous Bantu horticulturalist communities as well. <p>In 1996, Okapi Wildlife Reserve was named a UNESCO World Heritage Site, by virtue of it containing the most important and significant natural habitats for in-situ biodiversity conservation of threatened species. The reserve harbors globally important populations of African forest elephant, okapi, and eastern chimpanzees, as well as 13 other species of diurnal primate and four nocturnal species, ranking it as the top protected area in sub-Saharan Africa in terms of primate diversity.",
             location: {
                 center: [28.531101, 1.531721],
                 zoom: 7.5,
@@ -209,7 +209,7 @@ var config = {
             hidden: false,
             title: '4. Challenges in the Reserve',
             // image: './path/to/image/source.png',
-            description: 'The Reserve contains many small villages, and reserve management has the delicate task of engaging communities in conservation while limiting non-compatible land and resource use. The reserve has a history marred by spates of militarized elephant poaching, mining, and episodes of contact between park guards and militia groups, and the communities within the reserve remain very poor. Land and resource use within the reserve limit human migration and are meant to support only smallholder farming and sustainable, discriminate hunting. Despite these policies and decent management, the conservation outlook for the reserve remains critical as it has been retained on the List of World Heritage in Danger by UNESCO since 1997.',
+            description: "The Reserve contains many small villages, and reserve management has the delicate task of engaging communities in conservation while limiting non-compatible land and resource use. The reserve has a history marred by spates of militarized elephant poaching, mining, and episodes of contact between park guards and militia groups, and the communities within the reserve remain very poor. Land and resource use within the reserve limit human migration and are meant to support only smallholder farming and sustainable, discriminate hunting. Despite these policies and decent management, the conservation outlook for the reserve remains critical as it has been retained on the List of World Heritage in Danger by UNESCO since 1997.",
             location: {
                 center: [28.531101, 1.531721],
                 zoom: 8.5,
@@ -238,7 +238,7 @@ var config = {
             hidden: false,
             title: '5. Boundaries both real and imaginary',
             // image: './path/to/image/source.png',
-            description: 'While the Ministerial Decree creating the Reserve describes its boundaries in detail, portions of the boundaries follow artificial lines, such as the western boundary following 28 degrees East longitude, an imaginary line through the forest. Most boundaries follow natural features, such as the Nepoko River in the north.',
+            description: "While the Ministerial Decree creating the Reserve describes its boundaries in detail, portions of the boundaries follow artificial lines, such as the western boundary following 28 degrees East longitude, an imaginary line through the forest. Most boundaries follow natural features, such as the Nepoko River in the north.",
             location: {
                 center: [28.531101, 1.531721],
                 zoom: 8.5,
@@ -308,7 +308,7 @@ var config = {
             hidden: false,
             title: '7. The incongruence of mining concessions in a World Heritage site',
             // image: './path/to/image/source.png',
-            description: 'DRC’s Mining Cadastre (CAMI) has used its own version of the Reserve’s boundaries for at least 10 years, awarding and renewing research, exploration, and extraction concessions which technically fall inside of the Reserve’s decreed boundaries. These concessions are prevalent in allegedly productive areas along the Ituri River in the southern part of the reserve. Small-scale artisanal mining represented a persistent challenge in managing the Reserve’s southern bloc throughout the 2010s. On a few occasions, miners were cleared out from these mines located deep inside the forest, as were semi-industrial actors who brought in dredges to mine in the river.',
+            description: "DRC’s Mining Cadastre (CAMI) has used its own version of the Reserve’s boundaries for at least 10 years, awarding and renewing research, exploration, and extraction concessions which technically fall inside of the Reserve’s decreed boundaries. These concessions are prevalent in allegedly productive areas along the Ituri River in the southern part of the reserve. Small-scale artisanal mining represented a persistent challenge in managing the Reserve’s southern bloc throughout the 2010s. On a few occasions, miners were cleared out from these mines located deep inside the forest, as were semi-industrial actors who brought in dredges to mine in the river.",
             location: {
                 center: [28.531101, 1.531721],
                 zoom: 8.5,
@@ -385,7 +385,7 @@ var config = {
             hidden: false,
             title: '8. The persistent conservation challenge with artisanal mining',
             // image: '/assets/rfo_gfw_alerts_confidence.gif',
-            description: 'Small-scale artisanal mining represented a persistent challenge in managing the Reserve’s southern bloc throughout the 2010s, Due to its unregulated, illicit nature, artisanal mining leads to environmental damage, including deforestation and water pollution. Furthermore, in the Reserve’s delicate context, control of mining areas exacerbated conflict between different groups. Artisanal mining also impacts wildlife populations because miners tend to eat available bushmeat, which disincentivizes sustainable hunting in the Reserve. On a few occasions, park management cleared artisanal mines located deep inside the forest, as well as semi-industrial actors encroaching on the Reserve by dredging the Ituri River.',
+            description: "Small-scale artisanal mining represented a persistent challenge in managing the Reserve’s southern bloc throughout the 2010s, Due to its unregulated, illicit nature, artisanal mining leads to environmental damage, including deforestation and water pollution. Furthermore, in the Reserve’s delicate context, control of mining areas exacerbated conflict between different groups. Artisanal mining also impacts wildlife populations because miners tend to eat available bushmeat, which disincentivizes sustainable hunting in the Reserve. On a few occasions, park management cleared artisanal mines located deep inside the forest, as well as semi-industrial actors encroaching on the Reserve by dredging the Ituri River.",
             location: {
                 center: [28.14645, 1.31603],
                 zoom: 13,
@@ -435,7 +435,7 @@ var config = {
             hidden: false,
             title: '9. Development of a semi-industrial mining complex in the Okapi Reserve',
             // image: '/assets/rfo_gfw_alerts_confidence.gif',
-            description: 'In 2020, satellite imagery shows that an estimated 24 kilometers of new roads were opened to facilitate direct access to Muchacha, one of the largest mines in the southern part of the Reserve, and smaller sites south of the Ituri River. These roads enabled the moving of equipment to rapidly expand this and other mining sites. Massive forest clearing and the appearance of tailings ponds are clearly visible in imagery. This clearance allowed more dredges to access and mine in the Ituri River.',
+            description: "In 2020, satellite imagery shows that an estimated 24 kilometers of new roads were opened to facilitate direct access to Muchacha, one of the largest mines in the southern part of the Reserve, and smaller sites south of the Ituri River. These roads enabled the moving of equipment to rapidly expand this and other mining sites. Massive forest clearing and the appearance of tailings ponds are clearly visible in imagery. This clearance allowed more dredges to access and mine in the Ituri River.",
             location: {
                 center: [28.14645, 1.31603],
                 zoom: 13,
@@ -489,7 +489,7 @@ var config = {
             hidden: false,
             title: '10. More new roads and massive expansion in 2021',
             // image: '/assets/rfo_gfw_alerts_confidence.gif',
-            description: 'Small-scale artisanal mining represented a persistent challenge in managing the Reserve’s southwest bloc throughout the 2010s. On a few occasions, miners were cleared out from these mines located deep inside the forest. The satellite record showed dramatic expansion of the mines, first by clearing many kilometers of roads to access mines south of the Ituri River in 2020, then north of the Ituri River in 2021 and afterIn 2021, a similar pattern was mirrored on the north side of the Ituri River, where an estimated 17 kilometers or new roads were cleared to facilitate access, development, and expansion of mining sites. <p>Note the following statement from UNESCO in 2021, voting to retain Okapi Reserve on the List of World Heritage in Peril (<a href https://whc.unesco.org/en/decisions/7705): <p><i>“While noting the efforts undertaken to close artisanal mines, expresses serious concern that illegal mining in the property appears to be intensifying in reported complicity with certain local authorities, with small-scale artisanal mining operations evolving into large-scale semi-industrial operations, and the development of a permanent settlement at the Muchacha mining site.”',
+            description: "In 2021, a similar pattern was mirrored on the north side of the Ituri River, where an estimated 17 kilometers or new roads were cleared to facilitate access, development, and expansion of mining sites. <p>Note the following statement from UNESCO in 2021, regarding the <a href='https://whc.unesco.org/en/decisions/7705'>decision</a> to retain Okapi Reserve on the List of World Heritage in Peril: <p><i>`While noting the efforts undertaken to close artisanal mines, expresses serious concern that illegal mining in the property appears to be intensifying in reported complicity with certain local authorities, with small-scale artisanal mining operations evolving into large-scale semi-industrial operations, and the development of a permanent settlement at the Muchacha mining site.`</i>",
             location: {
                 center: [28.13123, 1.32219],
                 zoom: 12,
@@ -519,7 +519,7 @@ var config = {
             hidden: false,
             title: '11. Mine expansion continues unabated causing permanent damages',
             // image: '/assets/rfo_gfw_alerts_confidence.gif',
-            description: 'In 2022 and beyond, the mining complex appears to expand unabated. These mining sites are causing permanent damages to the forest, as well as the Ituri River for citizens downstream. There will undoubtedly be needed to plan for extensive restoration to ensure the natural environment continues to support wildlife, indigenous peoples of the Okapi Reserve.',
+            description: "In 2022 and beyond, the mining complex appears to expand unabated. These mining sites are causing permanent damages to the forest, as well as the Ituri River for citizens downstream. There will undoubtedly be needed to plan for extensive restoration to ensure the natural environment continues to support wildlife, indigenous peoples of the Okapi Reserve.",
             location: {
                 center: [28.10756, 1.32350],
                 zoom: 10.8,
